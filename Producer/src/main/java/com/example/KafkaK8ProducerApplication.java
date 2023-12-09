01ff16a0d01ea796f83340ecf1305b5db9ac6435
ldeeley <lester.deeley@yahoo.com>
2023-12-09 21:05:05 +0000

    Initial commit of Publisher Module

---
+++ Producer/src/main/java/com/example/KafkaK8ProducerApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaK8ProducerApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(KafkaK8ProducerApplication.class, args);
+        System.out.println("Hello world!");
+    }
+}
